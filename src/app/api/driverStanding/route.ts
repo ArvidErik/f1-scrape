c6ba4a0ee657a60d8e8125097535272795c8b351
ArvidErik <erik.arvid.hosszu@gmail.com>
2025-04-02 21:29:16 +0200

    pupeteer update

--- src/app/api/driverStanding/route.ts
+++ src/app/api/driverStanding/route.ts
@@ -1,33 +1,38 @@
 import puppeteer from "puppeteer";
 
 export async function GET() {
-  //@ts-ignore
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-  await page.goto("https://www.formula1.com/en/results/2025/drivers");
-
-  // Extract and map data into IDriverStanding format
-  const data = await page.evaluate(() => {
-    const rows = document.querySelectorAll(".f1-table tbody tr");
-
-    return Array.from(rows).map(row => {
-      const cells = row.querySelectorAll("td p"); // Selects <p> tags inside <td>
-
-      return {
-        //@ts-ignore
-        position: parseInt(cells[0]?.innerText.trim() || "0"),
-        //@ts-ignore
-        name: cells[1]?.innerText.trim() || "",
-        //@ts-ignore
-        nationality: cells[2]?.innerText.trim() || "",
-        //@ts-ignore
-        team: cells[3]?.innerText.trim() || "",
-        //@ts-ignore
-        points: parseFloat(cells[4]?.innerText.trim() || "0"),
-      };
+  try {
+    //@ts-ignore
+    const browser = await puppeteer.launch({ headless: "true" });
+    const page = await browser.newPage();
+    await page.goto("https://www.formula1.com/en/results/2025/drivers", { waitUntil: "domcontentloaded", timeout: 0 });
+  
+    // Extract and map data into IDriverStanding format
+    const data = await page.evaluate(() => {
+      const rows = document.querySelectorAll(".f1-table tbody tr");
+  
+      return Array.from(rows).map(row => {
+        const cells = row.querySelectorAll("td p"); // Selects <p> tags inside <td>
+  
+        return {
+          //@ts-ignore
+          position: parseInt(cells[0]?.innerText.trim() || "0"),
+          //@ts-ignore
+          name: cells[1]?.innerText.trim() || "",
+          //@ts-ignore
+          nationality: cells[2]?.innerText.trim() || "",
+          //@ts-ignore
+          team: cells[3]?.innerText.trim() || "",
+          //@ts-ignore
+          points: parseFloat(cells[4]?.innerText.trim() || "0"),
+        };
+      });
     });
-  });
-
-  await browser.close();
-  return Response.json({ result: data });
+  
+    await browser.close();
+    return Response.json({ result: data });
+  } catch (error:any) {
+    console.error("Error launching Puppeteer or fetching data:", error);
+    return Response.json({ error: error.message });
+  }
 }
