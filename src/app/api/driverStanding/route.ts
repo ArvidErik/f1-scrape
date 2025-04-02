604b4868dfff7964c03e35cc9eead43538093ef4
ArvidErik <erik.arvid.hosszu@gmail.com>
2025-04-02 23:31:15 +0200

    chromium test

--- src/app/api/driverStanding/route.ts
+++ src/app/api/driverStanding/route.ts
@@ -1,11 +1,14 @@
-import puppeteer from "puppeteer";
+import chromium from '@sparticuz/chromium-min';
+import puppeteer from 'puppeteer-core';
 
 export async function GET() {
   try {
     const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
       //@ts-ignore
-      headless: "true",
+      args: isLocal ? puppeteer.defaultArgs() : chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath('C:\Program Files\Google\Chrome\Application\chrome.exe'),
+      headless: chromium.headless,
     });
     const page = await browser.newPage();
     await page.goto("https://www.formula1.com/en/results/2025/drivers", { waitUntil: "domcontentloaded", timeout: 0 });
